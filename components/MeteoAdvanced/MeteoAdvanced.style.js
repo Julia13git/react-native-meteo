d695e164c5ce2888b26b9a3b8cff121bd2656e40
Yuliia Hots <goculiya@gmail.com>
2024-07-17 17:55:42 +0200

    ajout le composant meteoAdvanced

---
+++ components/MeteoAdvanced/MeteoAdvanced.style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+const s = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    backgroundColor: "#00000043",
+    borderRadius: 15,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    borderColor: "white",
+    borderWidth: 2,
+  },
+  label_box: {
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+  },
+  label: {
+    fontSize: 15,
+  },
+});
+export { s };
