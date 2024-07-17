891fb9164d4eb190bdb7d0bc43bcdab7463bdf55
Yuliia Hots <goculiya@gmail.com>
2024-07-17 11:46:48 +0200

    Les appels aux apis axios

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
+import { ImageBackground } from "react-native";
 import { s } from "./App.style";
 import Home from "./pages/Home/Home";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import backgroundImg from "./assets/background.png";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-        <Home />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ImageBackground
+      source={backgroundImg}
+      style={s.img_background}
+      imageStyle={s.img}
+    >
+      <SafeAreaProvider>
+        <SafeAreaView style={s.container}>
+          <Home />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ImageBackground>
   );
 }
--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -1,11 +1,52 @@
-import { Text } from "react-native";
-import {s} from "./Home.style";
+import { Text, View } from "react-native";
+import { s } from "./Home.style";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
+import { useEffect, useState } from "react";
+import { MeteoAPI } from "../../api/meteo";
 
 export default function Home() {
+  const [coords, setCoords] = useState();
+  const [weather, setWeather] = useState();
+
+  useEffect(() => {
+    getUserCoords();
+  }, []);
+
+  useEffect(() => {
+    if (coords) {
+      fetchWeather(coords);
+    }
+  }, [coords]);
+
+  async function getUserCoords() {
+    let { status } = await requestForegroundPermissionsAsync();
+    if (status === "granted") {
+      const location = await getCurrentPositionAsync();
+      setCoords({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      });
+    } else {
+      setCoords({ lat: "48.85", lng: "2.35" });
+    }
+  }
+
+  async function fetchWeather(coordinates) {
+    const weatherResponse = await MeteoAPI.fetchWeatherFromCoords(coordinates);
+    setWeather(weatherResponse);
+  }
+
+  console.log(weather);
   return (
-   <>
-   <Text>Home</Text>
-   </>
+    <>
+      <View style={s.meteo_basic}>
+        <Text>Hello</Text>
+      </View>
+      <View style={s.searchbar_container}></View>
+      <View style={s.meteo_advanced}></View>
+    </>
   );
 }
-
