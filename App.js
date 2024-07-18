aa6341eb3efe1e9831474d8062b661fbd2d5e4ba
Yuliia Hots <goculiya@gmail.com>
2024-07-18 10:46:29 +0200

    Ajout de la route forecast

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import AlataRegular from "./assets/fonts/Alata-Regular.ttf";
 import { useFonts } from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Forecast } from "./pages/Forecast/Forecast";
 const Stack = createNativeStackNavigator();
 
 const navTheme = {
@@ -32,9 +33,10 @@ export default function App() {
             {isFontLoaded ? (
               <Stack.Navigator
                 screenOptions={{ headerShown: false }}
-                initialRouteName="Home"
+                initialRouteName="Forecast"
               >
                 <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Forecast" component={Forecast} />
               </Stack.Navigator>
             ) : null}
           </SafeAreaView>
