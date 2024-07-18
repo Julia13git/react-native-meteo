3b3466a11e555539d869d9f73d0eaf8ecb7a7547
Yuliia Hots <goculiya@gmail.com>
2024-07-18 12:04:55 +0200

    Creation du contenu de la page forecast

--- pages/Forecast/Forecast.jsx
+++ pages/Forecast/Forecast.jsx
@@ -1,14 +1,26 @@
 import { s } from "./Forecast.style";
 import { Txt } from "../../components/Txt/Txt";
 import { Container } from "../../components/Container/Container";
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { TouchableOpacity, View } from "react-native";
 
 export function Forecast({}) {
   const { params } = useRoute();
+  const nav = useNavigation();
 
-  return (
-    <Container>
-      <Txt>Forecast</Txt>
-    </Container>
+  const backButton = (
+    <TouchableOpacity style={s.back_btn} onPress={() => nav.goBack()}>
+      <Txt>{"<"}</Txt>
+    </TouchableOpacity>
   );
+  const header = (
+    <View style={s.header}>
+      {backButton}
+      <View style={s.header_texts}>
+        <Txt>{params.city}</Txt>
+        <Txt style={s.subtitle}>Prévision sur 7 jours</Txt>
+      </View>
+    </View>
+  );
+  return <Container>{header}</Container>;
 }
